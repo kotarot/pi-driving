992bd4eba894a92f22abc21048ba80a6e99d22de
KazushiKawamura <kazushi.kawamura@togawa.cs.waseda.ac.jp>
2016-06-13 15:15:58 +0900

    [Raspberry] Update at 2016-06-13

--- client/car_raspberry.py
+++ client/car_raspberry.py
@@ -6,10 +6,23 @@ import multiprocessing
 import time
 import threading
 import urllib2
+import RPi.GPIO as GPIO
 
 ID = 'raspberry' # <- チームのIDを入れる
 URL = 'http://www.togawa.cs.waseda.ac.jp/~kotaro.terada/pi/data/' + ID + '.json'
 
+PIN1 = 11
+PIN2 = 13
+PIN3 = 19
+PIN4 = 21
+
+GPIO.setmode(GPIO.BOARD)
+# right
+GPIO.setup(PIN1, GPIO.OUT)
+GPIO.setup(PIN2, GPIO.OUT)
+# left
+GPIO.setup(PIN3, GPIO.OUT)
+GPIO.setup(PIN4, GPIO.OUT)
 
 # ここに取得データを元にして対象デバイスを動作させる処理を書く
 def worker(input):
@@ -17,13 +30,29 @@ def worker(input):
     right_tire = input['gamma']
     left_tire = input['gamma']
 
-    if input['beta'] > 0:
-        left_tire = input['gamma']*input['beta']
-    elif input['beta'] < 0:
-        right_tire = input['gamma']*(-1)*input['beta']
+    if input['beta'] > 20:
+        #left_tire = input['gamma']*input['beta']
+        GPIO.output(PIN1, True)
+        GPIO.output(PIN2, False)
+        GPIO.output(PIN3, False)
+        GPIO.output(PIN4, False)
+        print 'left'
+    elif input['beta'] < -20:
+        #right_tire = input['gamma']*(-1)*input['beta']
+        GPIO.output(PIN1, False)
+        GPIO.output(PIN2, False)
+        GPIO.output(PIN3, True)
+        GPIO.output(PIN4, False)
+        print 'right'
+    else:
+        GPIO.output(PIN2, False)
+        GPIO.output(PIN1, True)
+        GPIO.output(PIN4, False)
+        GPIO.output(PIN3, True)
+        print 'straight'
 
     # 取得値を表示
-    print left_tire , right_tire
+    #print left_tire , right_tire
 
     
     # 早過ぎたら適当にスリープする
